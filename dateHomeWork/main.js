683477c90857837115edea2c71f85b7e424c2d60
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-05-23 16:48:51 +0300

    Add files via upload

---
+++ dateHomeWork/main.js
@@ -0,0 +1,128 @@
+//49
+// function dateAndHour(){
+//     var time=new Date();
+//     console.log(time,time.getHours())
+// }
+// dateAndHour();
+
+//50
+// function dateString(){
+//     var time=new Date("12/31/1993");
+//     console.log(time)
+// }
+// dateString();
+
+//51
+// function dateString(){
+//     var time=new Date(prompt("enter date"));
+//     console.log(time)
+// }
+// dateString();
+
+//52
+// function dateString(){
+//      var time=new Date(prompt("enter date"));
+// console.log(time.getMonth())
+// }
+// dateString();
+
+//53
+// function hourOfDate(){
+// var time=new Date(prompt("enter date"));
+// console.log(time.getHours());
+// }
+// hourOfDate();
+
+//54
+// function yesrOfDate(){
+// var time=new Date(prompt("enter date"));
+// console.log(time.getFullYear());
+// }
+// yesrOfDate();
+
+//55
+// function minutsOfDate(){
+//     var time=new Date(prompt("enter date"));
+//     console.log(time.getMinutes());
+//     }
+//     minutsOfDate();
+
+//56
+
+// function twoDates(){
+//     var one=new Date(prompt("enter date one"));
+//     var two=new Date(prompt("enter date two"));
+// if(one>two){
+//     return console.log(two);
+// }
+// else
+// return console.log(one);
+// }
+// twoDates();
+
+//57
+// function ageOfuser(){
+//   var age=new Date(prompt("enter date"));
+//   var age2=new Date();
+
+//     return console.log(age2.getFullYear()-age.getFullYear());
+// }
+// ageOfuser();
+
+//58
+
+// function overEighting(){
+//     var age=new Date(prompt("enter date"));
+//     var age2=new Date();
+//     var result=age2.getFullYear()-age.getFullYear();
+
+//     if(result>=18){
+//         alert("you can enter ")
+//     }
+//     else
+//     console.log("you cant enter"+result)
+
+// }
+//59
+// function locationOfuser(){
+//   var age=new Date(prompt("enter date"));
+
+//     return console.log(age.toLocaleDateString());
+// }
+// locationOfuser();
+//60
+function randomDte(){
+    var x=getrnd(1,12)+"/"+getrnd(1,31)+"/"+getrnd(1990,2022)
+
+    var datee=new Date(x)
+    console.log(datee);
+
+
+}
+
+randomDte();
+
+function getrnd(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  getrnd();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
